0a4d6df47fe305fa06675a9d823189fb37304c98
Silas Henrique <silas.cerqueira@equatorialenergia.com.br>
2025-08-13 08:52:17 -0300

    Select between helvetica and courier

--- pytext.py
+++ pytext.py
@@ -19,4 +19,22 @@ def saveas():
 button = Button(root, text="Save", command=saveas)
 button.grid()
 
+def FontHelvetica():
+    global text
+    text.config(font="Courier")
+def FontCourier():
+    global text
+    text.config(font="Courier")
+
+
+font = Menubutton(root, text="Font")
+font.grid()
+font.menu=Menu(font, tearoff=0)
+font["menu"] = font.menu 
+Helvetica = IntVar()
+times = IntVar()
+Courier = IntVar()
+font.menu.add_checkbutton(label="Courier", variable=Courier, command=FontCourier)
+font.menu.add_checkbutton(label="Helvetica", variable=Helvetica, command=FontHelvetica)
+
 root.mainloop()
